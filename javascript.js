2e1acdc9665b943d72be26589afb80a8f4a00d3f
mshivab <106754602+mshivab@users.noreply.github.com>
2025-01-18 15:40:22 +0530

    Update javascript.js
    Added a disp function with alert message - 'Kindly switch to 'Desktop site' mode if you're viewing on mobile Chrome...'

--- javascript.js
+++ javascript.js
@@ -2,3 +2,8 @@ function contact()
 {
     alert("Contact - 6360191005/9663676225 \nEmail - mshivaprasad303@gmail.com \nApplication Development in Progress..!");
 }
+
+function disp()
+{
+    alert("Kindly switch to 'Desktop site' mode if you're viewing on mobile Chrome...")
+}
